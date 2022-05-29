f31149b520d62d7c64432b3daf9a9f29e6d677c4
V.Meganathan <meganathan.vjms@gmail.com>
2022-05-29 18:54:04 +0530

    MongoDB

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const dotenv = require("dotenv")
+dotenv.config()
 
 //express app
 const app = express();
 
-//register view engine
+//connect to mongodb
+const dbURI='mongodb+srv://'+process.env.user+':'+process.env.pwd+'@nodetuts.gzgrtrc.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {app.listen(3000)})
+  .catch((err) => {console.log(err)});
+
+  //register view engine
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+//routes
 app.get('/',(req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index',{ title:'Home', blogs});  
+  res.redirect('/blogs')
 });
 
 
 app.get('/about',(req, res) => {
   res.render('about',{ title:'About'});  
 });
+
+//blog routes
+app.get('/blogs',(req, res) => {
+  Blog.find().sort({createdAt:-1})
+  .then((result) => {
+    res.render('index',{title:'All Blogs',blogs:result});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
 
 app.get('/blogs/create',(req, res) => {
   res.render('create',{ title:'Create a new blog'});
