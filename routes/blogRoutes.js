6f72917d0bb5ee14ae095670c6f6b58e8b7c2c5b
V.Meganathan <meganathan.vjms@gmail.com>
2022-05-30 07:54:55 +0530

    MVC restructuring

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,48 +1,16 @@
-const express=require('express');
-const Blog = require('../models/blog');
+const express = require('express');
+const blogController = require('../controllers/blogController');
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/create',(req, res) => {
-  res.render('create',{ title:'Create a new blog'});
-})
+router.get('/create', blogController.blog_create_get);
 
-router.get('/',(req, res) => {
-  Blog.find().sort({createdAt:-1})
-  .then((result) => {
-    res.render('index',{title:'All Blogs',blogs:result});
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
+router.get('/', blogController.blog_index);
 
-router.post('/',(req, res) => {
-  const blog = new Blog(req.body);
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch(err => console.log(err));
-})
+router.post('/', blogController.blog_create_post);
 
-router.get('/:id',(req, res) => {
-  const id=req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details',{blog:result,title: 'Blog Details'});
-    })
-    .catch(err => console.log(err));
-})
+router.get('/:id', blogController.blog_details);
 
-router.delete('/:id',(req, res) => {
-  const id=req.params.id;
+router.delete('/:id', blogController.blog_delete)
 
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({redirect: '/blogs'});
-    })
-    .catch(err => console.log(err));
-})
-
-module.exports=router;
+module.exports = router;
