b30bd9d5153692bc1880aa7496dd4221e44e5643
V.Meganathan <meganathan.vjms@gmail.com>
2022-05-30 08:41:11 +0530

    Favicon and 404s

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -19,7 +19,9 @@ const blog_details = (req, res) => {
     .then((result) => {
       res.render('blogs/details', { blog: result, title: 'Blog Details' });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(404).render('404',{ title:'Blog not found' });
+    });
 }
 
 const blog_create_get = (req, res) => {
